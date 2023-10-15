5fe550951c5d9a79f0be53cdd5bd1c0470c6d0ca
Calvin <CalvinCui@foxmail.com>
2023-10-15 20:59:59 +0800

    implementation of python

--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 
 Naive implementation of mendelbrot calculation with different programming languages.
 
+What is the Mandelbrot set?
+The Mandelbrot set consists of all (complex numbers) c that satisfy a certain condition: a complex number c such that the trajectory produced by iterating through x² + c starting from an initial value of 0 does not go to infinity.
+
+#### Usage of  each scripts
+
+```shell
+${run command} ${xmin} ${xmax} ${ymin} ${ymax} ${width} ${height} ${maxiter}
+```
+
 #### Todo list
 
 ##### Languages
 
 - [ ] C/C++
-
 - [ ] Rust
-- [ ] Python
+- [x] Python
 - [ ] Java
 - [ ] Javascript
 - [ ] Go
---
+++ mandel.py
@@ -0,0 +1,77 @@
+import sys
+from typing import List
+
+class Complex:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Complex(self.x + other.x, self.y + other.y)
+    
+    def __mul__(self, other):
+        return Complex(self.x * other.x - self.y * other.y,
+                       self.x * other.y + self.y * other.x)
+
+    def __abs__(self):
+        return abs(self.x * self.x - self.y * self.y) #square of the actual mod
+
+def linspace(vmin: float, vmax: float, length: int) -> List[float]:
+    step = (vmax - vmin) / (length - 1)
+    ret = []
+    i = 0
+    v = vmin
+    while i < length:
+        ret.append(v)
+        v += step
+        i += 1
+    return ret
+
+def participants(X: List[float], Y: List[float]) -> List[Complex]:
+    ret = []
+    for y in Y:
+        for x in X:
+            ret.append(Complex(x, y))
+    return ret
+
+def calc(C: List[Complex], maxiter: int, threshold: int = 4) -> List[int]:
+    ret = []
+    for c in C:
+        z = Complex(0, 0)
+        for i in range(maxiter):
+            z = z * z + c
+            if abs(z) > threshold:
+                break
+        ret.append(i)
+    return ret
+
+def run(xmin: float, xmax: float, ymin: float, ymax: float, width: int, height: int, maxiter: int):
+    X = linspace(xmin, xmax, width)
+    Y = linspace(ymin, ymax, height)
+    C = participants(X, Y)
+    steps = calc(C, maxiter)
+    return steps
+
+def plot(steps: List[int], width, height):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    steps = np.array(steps)
+    steps = steps.reshape((height, width))
+    plt.imshow(steps, interpolation="nearest")
+    plt.show()
+
+def test(
+        xmin = -0.22,
+        xmax = -0.219,
+        ymin = -0.70,
+        ymax = -0.699,
+        width = 100,
+        height = 100,
+        maxiter = 124
+    ):
+    steps = run(xmin, xmax, ymin, ymax, width, height, maxiter)
+    plot(steps, width, height)
+
+if __name__ == "__main__":
+    xmin, xmax, ymin, ymax, width, height, maxiter = sys.argv[1:6]
+    run(xmin, xmax, ymin, ymax, width, height, maxiter)
